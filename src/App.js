aeb2097e6c611fcf1ad1ada22652967625831ec9
Shreshth Mishra <shreshth.mishra@bizongo.com>
2024-02-20 08:10:22 +0000

    few fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Top from './components/Top.js'
-import Side from './components/Side.js';
+import React from "react";
+import Top from "./components/Top.js";
+import Side from "./components/Side.js";
+
 function App() {
-  
   return (
-    <div className="App">       
-            <Top />  
-            <Side />
+    <div className="App">
+      <Top />
+      <Side />
     </div>
   );
 }
