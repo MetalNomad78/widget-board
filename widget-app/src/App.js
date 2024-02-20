421b5a12fd1479e4875a010a43ccf45cdc5a95a1
MetalNomad78 <singhsiddhant9760@gmail.com>
2024-02-21 04:07:32 +0530

    first widget

---
+++ widget-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNavWidget from './components/TopNavWidget';
+
+function App() {
+  return (
+    <div>
+      <TopNavWidget />
+    </div>
+  );
+}
+
+export default App;
---
+++ widget-app/src/components/CustomTable.js
@@ -0,0 +1,80 @@
+import { AgGridReact } from 'ag-grid-react';
+import "./ag-grid.css";
+import "./ag-theme-quartz.css";
+import { useState } from 'react';
+import { AgChartsReact } from 'ag-charts-react';
+
+const CustomTable = ({backgroundColor,footerColor,type}) => {
+  const [rowData] = useState([
+    { product: "Reusable", q1_23: 10, q2_24: 4 },
+    { product: "Natural", q1_23: 2, q2_24: 11 },
+    { product: "Compost", q1_23: 7, q2_24: 5 },
+    { product: "Reusable..", q1_23: 3, q2_24: 4 },
+    { product: "Total", q1_23: 8, q2_24: 12 },
+  ]);
+
+  const percentageData = rowData.map(item => ({
+    product: item.product,
+    q1_23: `${item.q1_23}%`,
+    q2_24: `${item.q2_24}%`,
+  }));
+
+  const rowStyle = { borderBottom: 'none' };
+
+  // set background color and space below even rows again, this looks bad, should be using CSS classes
+  const getRowStyle = params => {
+    if (params.node.rowIndex === 4) {
+      return { color: footerColor, fontWeight: 'bold'};
+    }
+  
+  };
+
+  const [colDefs] = useState([
+    { headerName: "PRODUCT", field: "product", width: 150 },
+    { headerName: "Q1-23", field: "q1_23", width: 100, headerClass: 'header-cell', footerClass: 'total-cell' },
+    { headerName: "Q2-24", field: "q2_24", width: 100, headerClass: 'header-cell', footerClass: 'total-cell' },
+  ]);
+  const [chartOptions] = useState({
+    theme: "ag-default",
+    // Data: Data to be displayed in the chart
+    data: [
+      { asset: "Stocks", amount: 60000 },
+      { asset: "Bonds", amount: 40000 },
+      { asset: "Cash", amount: 7000 },
+      { asset: "Real Estate", amount: 5000 },
+      { asset: "Commodities", amount: 3000 },
+    ],
+    // Series: Defines which chart type and data to use
+    series: [
+      {
+          type: 'donut',
+          calloutLabelKey: 'asset',
+          angleKey: 'amount',
+          innerRadiusRatio: 0.7,
+          labelKey: '',
+      },
+  ],
+  background: {
+    fill: backgroundColor,
+  },
+  });
+
+  const customGridWidth = '360px';
+  if(type==='grid'){
+    return (
+      <div className={`ag-theme-quartz`} style={{ height: 200, width: customGridWidth, '--custom-background-color': backgroundColor }}>
+        <AgGridReact rowStyle={rowStyle} getRowStyle={getRowStyle} rowData={percentageData} columnDefs={colDefs} />
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className={`styles.css`} style={{height: 250, width: 300}}>
+      <AgChartsReact options={chartOptions} />
+      </div>
+    );
+  }
+  
+};
+
+export default CustomTable;
