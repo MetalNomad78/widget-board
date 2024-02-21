0e77c099b38e79517bb56b64633f5a930ac86133
Shreshth Mishra <shreshth.mishra@bizongo.com>
2024-02-21 09:13:49 +0000

    Use widget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Top from "./components/Top.js";
 import Side from "./components/Side.js";
+import TopNavWidget from "./components/TopNavWidget.js";
 
 function App() {
   return (
     <div className="App">
       <Top />
+      <TopNavWidget/>
       <Side />
     </div>
   );
